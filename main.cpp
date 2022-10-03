e0dd1019364faa9b4287558ffee1e29815261c08
maru0522 <k021g1258@g.neec.ac.jp>
2022-10-03 10:42:54 +0900

    main内の引数2を'b'に変更した。

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main(void) {
     printf("%d\n", Min<int>(100, 200));
     printf("%f\n", Min<float>(10.2f, 20.8f));
     printf("%lf\n", Min<double>(10.22f, 20.88f));
-    printf("%c\n", Min<char>('a', 'a'));
+    printf("%c\n", Min<char>('a', 'b'));
 
     return 0;
 }
