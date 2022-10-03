9e348ebf9eaa07e1bfada8e4c70fd155c02697f1
maru0522 <k021g1258@g.neec.ac.jp>
2022-10-03 10:28:59 +0900

    1-1.オーバーロードと関数テンプレート課題

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+template<class T>
+T Min(T a, T b) {
+    if (a <= b) {
+        return a;
+    }
+
+    return b;
+}
+
+template<>
+char Min<char>(char a, char b) {
+    printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñB");
+    return 0;
+}
+
+int main(void) {
+    printf("%d\n", Min<int>(100, 200));
+    printf("%f\n", Min<float>(10.2f, 20.8f));
+    printf("%lf\n", Min<double>(10.22f, 20.88f));
+    printf("%c\n", Min<char>('a', 'a'));
+
+    return 0;
+}
