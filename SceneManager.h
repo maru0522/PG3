f699bfb634043c203130b4ee9505afbd60c9b86b
maru0522 <94513876+maru0522@users.noreply.github.com>
2022-11-21 14:47:49 +0900

    3-1

---
+++ SceneManager.h
@@ -0,0 +1,28 @@
+#pragma once
+class SceneManager
+{
+public: // ’è‹`
+    enum Scene
+    {
+        Title,
+        NewGame,
+        GamePlay,
+        GameClear
+    };
+
+public: // Ã“IŠÖ”
+    static SceneManager* GetInstance(void);
+
+public: // ŠÖ”
+    void ChangeScene(int nextScene) { currentScene_ = nextScene; }
+    void Draw(void);
+
+public: // •Ï”
+    int currentScene_{ 0 };
+
+private: // singleton
+    SceneManager(void) = default;
+    ~SceneManager(void) = default;
+    SceneManager(const SceneManager & ins) = delete;
+    SceneManager& operator=(const SceneManager & ins) = delete;
+};
--- main.cpp
+++ main.cpp
@@ -1,47 +1,23 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <time.h>
+#include "SceneManager.h"
 #include <iostream>
-#include <functional>
-
-// コールバック関数
-void Dice(int num) {
-    srand(time(nullptr));
-
-    int randNum{ rand()%6 + 1 };
-    std::cout << "サイコロの値は" << randNum << "でした。" << std::endl;
-
-    if (num % 2 == randNum % 2) {
-        std::cout << "あなたの勝ち。" << std::endl;
-    }
-    else {
-        std::cout << "あなたの負け。" << std::endl;
-    }
-}
 
 int main(void) {
-    int num{ 0 };
 
-    std::cout << "偶数/奇数どちらになるか予想し値を入力してください。" << std::endl;
-    std::cout << "偶数なら[0]を、奇数なら[1]を入力してください。" << std::endl;
+    SceneManager* sceneM = SceneManager::GetInstance();
 
-    std::cin >> num;
+    int num{};
 
-    std::function<void(int)> setTimeout = [=](int second){
-        std::cout << second << "秒待ってください。" << std::endl;
-        // 待機
-        Sleep(second * 1000);
-        Dice(num);
-    };
+    while (sceneM->currentScene_ < 4)
+    {
+        sceneM->Draw();
 
-    // 0か1を必ず入力させる
-    while (num != 0 && num != 1) {
-        std::cout << "入力された値が不適切です。偶数なら[0]を、奇数なら[1]を入力してください。" << std::endl;
+        std::cout << "enter next scene number" << std::endl;
         std::cin >> num;
+        sceneM->ChangeScene(num);
     }
-
-    // 待機時間[s], 予想された値
-    setTimeout(3);
 
     system("pause");
     return 0;
